6ff230a38e53ec9686a917646d3e6aa7b9625bb6
Scoorch <trevorchen195@gmail.com>
2024-09-11 13:30:55 -0700

    Add files via upload

--- ap/blueJIntro/inputOutput.java
+++ ap/blueJIntro/inputOutput.java
@@ -8,31 +8,33 @@ import java.util.Scanner;
 
 public class inputOutput {
     public static void main() {
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.print("What is your favorite course at school? ");
-        String c = sc.nextLine();
+        String c = s.nextLine();
         System.out.print("What is your favorite show? ");
-        String s = sc.nextLine();
+        String sh = s.nextLine();
         System.out.print("How many hours do you spend studying each day? ");
-        int hst = sc.nextInt();
+        int hst = s.nextInt();
         System.out.print("How many hours do you spend watching shows each day? ");
-        int hsh = sc.nextInt();
+        int hsh = s.nextInt();
         int ht = hst+hsh;
-        
+
         System.out.println();
         System.out.println("Summary:");
         System.out.println("Your favorite course is "+c+".");
-        System.out.println("Your favorite show is "+s+".");
+        System.out.println("Your favorite show is "+sh+".");
         System.out.println("You spend "+hst+" hours studying each day.");
         System.out.println("You spend "+hsh+" hours watching shows each day.");
         System.out.println("In total, you spend "+ht+" hours not touching grass each day.");
-        String r = "a";
+        String r;
+        System.out.print("Repeat? Y/N: ");
+        r = s.next();
         while (r.equals("Y") && r.equals("y")) {
             System.out.print("Recalculate? Y/N: ");
-            r = sc.next();
+            r = s.next();
         }
         if (r.equals("Y") || r.equals("y")) {
-            sc.close();
+            s.close();
             main();
         }
     }
--- ap/calculations/template.java
+++ ap/calculations/template.java
@@ -4,8 +4,12 @@
  * 24/9/10
  */
 
+import java.util.Scanner;
+
 public class template {
     public static void main() {
+        Scanner s = new Scanner(System.in);
         
+        s.close();
     }
 }
