d3c4e04a355bbbe711b77201a39780feedbd7d5e
Scoorch <trevorchen195@gmail.com>
2024-10-30 12:24:02 -0700

    Add files via upload

---
+++ ap/Problem Set 1/blueJIntro/printVsPrintln.java
@@ -0,0 +1,14 @@
+/**
+ * BlueJ Intro
+ * Trevor Chen
+ * 24/9/6
+ */
+
+public class printVsPrintln {
+    public static void main() {
+        System.out.print("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.print("D");
+    }
+}
